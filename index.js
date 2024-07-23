3efe53a9d1be26a9359a909ab2bdba20dc713d47
JustinBenito <justinbenito1974@gmail.com>
2024-07-23 11:20:40 +0530

    please save me

--- index.js
+++ index.js
@@ -14,42 +14,29 @@ const speechConfig = sdk.SpeechConfig.fromSubscription(
   process.env.SPEECH_REGION
 );
 
-const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid'); // To generate unique file names
-
 app.post("/tts", async (req, res) => {
-  const inputText = req.body.text;
   const speechSynthesizer = new sdk.SpeechSynthesizer(speechConfig);
   const ssml = `<speak version='1.0' xml:lang='en-US' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts'> \r\n \
   <voice name='ta-IN-PallaviNeural'> \r\n \
       <prosody rate='-100%' > \r\n \
           <mstts:viseme type='redlips_front'/> \r\n \
-          ${inputText} \r\n \
+          'அம்மா', \r\n \
       </prosody> \r\n \
   </voice> \r\n \
 </speak>`;
 
   speechSynthesizer.speakSsmlAsync(
     ssml,
-    async (result) => {
+    (result) => {
       const { audioData } = result;
       speechSynthesizer.close();
 
       const wavBuffer = Buffer.from(audioData);
-      const fileName = `${uuidv4()}.wav`;
-      const filePath = path.join(__dirname, 'public', 'audio', fileName);
-
-      fs.writeFile(filePath, wavBuffer, async (err) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send("Error saving audio file");
-        } else {
-          // Assuming your server serves files from 'public/audio' directory
-          const fileUri = `https://localhost:3002/audio/${fileName}`;
-          res.json({ audioUri: fileUri });
-        }
-      });
+      console.log(wavBuffer.length);
+      res.setHeader("Content-Type", "audio/wav");
+      res.setHeader("Content-Disposition", 'attachment; filename="output.wav"');
+      res.setHeader("Content-Length", wavBuffer.length);
+      res.send(wavBuffer);
     },
     (error) => {
       console.log(error);
@@ -58,7 +45,6 @@ app.post("/tts", async (req, res) => {
     }
   );
 });
-
 
 app.post("/viseme", async (req, res) => {
   const inputText = req.body.text;
